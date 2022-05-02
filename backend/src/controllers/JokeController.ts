3f8d58994f73c792c14f982b56e449481f9a544e
Sylvn001 <juniorbaarros@gmail.com>
2022-05-02 13:22:43 -0300

    fix: backend routes

--- backend/src/controllers/JokeController.ts
+++ backend/src/controllers/JokeController.ts
@@ -1,21 +1,19 @@
 import { Request, Response } from "express";
-import {connectToDatabase} from '../database/db';
+import { connectToDatabase } from "../database/db";
 
-
-export default class JokeController{
-  async findAll(request: Request, response: Response){
+export default class JokeController {
+  async findAll(request: Request, response: Response) {
     const dbCOn = await connectToDatabase();
-    const filter = request.params.filter || {}
-    const query = {"titulo":filter };
-    const data = dbCOn.collection("piadas").find(query);
-    response.status(200).json(data)
+    const filter = request.params.filter;
+    const query = filter ? { titulo: filter } : {};
+    const data = await dbCOn.collection("piadas").find(query).toArray();
+    response.status(200).json(data);
   }
 
-  async create(request: Request, response: Response){
+  async create(request: Request, response: Response) {
     const dbCOn = await connectToDatabase();
     const joke = request.body;
-    const data = await dbCOn.collection("piadas").insertOne(joke)
-    return response.status(200).json(data)
+    const data = await dbCOn.collection("piadas").insertOne(joke);
+    return response.status(200).json(data);
   }
-
 }
